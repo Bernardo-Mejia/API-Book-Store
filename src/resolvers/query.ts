cb90e3c91a1400b98273903cb16d6ec1899dc722
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2023-12-15 10:51:51 -0600

    Return status and message in Book Query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -17,16 +17,42 @@ const QueryResolver: IResolvers = {
       return `Hola, ${args.name}`;
     },
     peopleNumber: () => 703,
-    booksList: (): Array<IBook> => {
-      return data.books;
+    booksList: (): {
+      status: boolean;
+      message: string;
+      list: Array<IBook>;
+    } => {
+      return {
+        status: true,
+        message: "Lista de libros cargada correctamente",
+        list: data.books,
+      };
     },
     peoplesList: (): Array<IPeople> => {
       return data.people;
     },
-    book: (_: void, args: { id: string }) => {
-      return data.books.filter((value: IBook) => value.id === args.id)[0];
+    book: (
+      _: void,
+      args: { id: string }
+    ): {
+      status: boolean;
+      message: string;
+      item?: IBook;
+    } => {
+      // return
+      const SearchBook: IBook = data.books.filter(
+        (value: IBook) => value.id === args.id
+      )[0];
+      return {
+        status: SearchBook !== undefined ? true : false,
+        message:
+          SearchBook !== undefined
+            ? `Book founded with id ${args.id}`
+            : `Book not founded with id ${args.id}`,
+        item: SearchBook,
+      };
     },
-    people: (_: void, args: { id: string }) => {
+    people: (_: void, args: { id: string }): IPeople => {
       return data.people.filter((value: IPeople) => value.id === args.id)[0];
     },
   },
