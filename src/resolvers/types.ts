0e3c560fd1546ebe66a21dc1bf04dc3c4f658247
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2023-12-23 22:57:03 -0600

    People and Books Types

--- src/resolvers/types.ts
+++ src/resolvers/types.ts
@@ -1,4 +1,7 @@
 import { IResolvers } from "@graphql-tools/utils";
+import data from "../data";
+import { IBook } from "../interfaces/book-interface";
+import { IPeople } from "../interfaces/people-interface";
 const typesResolver: IResolvers = {
   Data: {
     __resolveType(obj: { name: string; isbn: string }) {
@@ -11,6 +14,21 @@ const typesResolver: IResolvers = {
         return "Book";
       }
       return null; // GraphQLError is thrown
+    },
+  },
+  People: {
+    booksBuy: (root: { books: Array<string> }) => {
+      // console.log("<===========root===========>", root.books);
+      return data.books.filter(
+        (book: IBook) => root.books.indexOf(book.id) > -1
+      );
+    },
+  },
+  Book: {
+    byPeopleBuy: (root: { id: string }) => {
+      return data.people.filter(
+        (people: IPeople) => people.books.indexOf(root.id) > -1
+      );
     },
   },
 };
